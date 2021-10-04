386f4f45b5520a2746fd4b21e3348f93dd0d7d70
BryanUGB <91899351+BryanUGB@users.noreply.github.com>
2021-10-04 10:34:05 -0600

    Creamos vista lista de producto

--- Uniones y vistas.sql
+++ Uniones y vistas.sql
@@ -1,1 +1,28 @@
-USE Northwind
+USE Northwind;
+
+SELECT 
+      p.ProductName producto,
+	  c.CategoryName categoria
+FROM
+	Products p
+INNER JOIN	Categories C
+ON p.CategoryID= c.CategoryID;
+
+--Crear una vista que muestre el nombre del producto 
+--categoria  y proveedor  ordenados por nombre de categoria
+
+
+CREATE VIEW Listaproducto AS 
+SELECT
+	p.ProductName producto,
+	c.CategoryName categoria,
+	u.ContactName proveedor
+
+FROM Products p
+INNER JOIN Categories C
+ON p.CategoryID= c.CategoryID
+INNER JOIN Customers u
+ON CustomerID= u.CustomerID
+
+
+
